6888f13aae735b13644ea070b222a8a9fb8d0e74
Eduardo <eocament@gmail.com>
2022-02-21 20:34:36 -0300

    Segunda version Desafio Multiplicación

--- assets/js/mult.js
+++ assets/js/mult.js
@@ -1,8 +1,8 @@
 
-// Scipts JS llamado desde Index.html
+// Scripts JS llamado desde Index.html
 // Función que valida número ingresado por usuario
  let validacion = (n) => {
-  while (n <= 0 || n > 20){
+  while (n < 1 || n > 20){
       n = parseInt(prompt("Número fuera de rango!!"));
    }
    return n;
@@ -12,7 +12,11 @@
 // Función que calcula la multiplicación
 let mul = (n) => {
   for(let i = 1; i <= n; i++){
-    document.write(i + " X " + n + " = " + i*n + "<br>");
+
+    // Las salidas por pantalla se lleva por consola de acuerdo a lo solicitadp
+    // al mismo tiempo se despiega mediante document.write
+   document.write(i + " X " + n + " = " + i*n + "<br>");
+     console.log(i + " X " + n + " = " + i*n + "<br>");
   }
 }
 
@@ -25,11 +29,15 @@ fac = (n) => {
     for(j = 1; j <= i; j++){
         factorial = factorial * j;
     }
+
+    // Las salidas por pantalla se lleva por consola de acuerdo a lo solicitadp
+    // al mismo tiempo se despiega mediante document.write
     document.write("Factorial de " + i + " es: " + factorial + "<br>");
+    console.log("Factorial de " + i + " es: " + factorial + "<br>");
   }
 }
 
 let numero = parseInt(prompt("Escriba un número entre 1 y 20"));
-numero = validacion(numero);
-mul(numero);
-fac(numero);
+let num = validacion(numero);
+mul(num);
+fac(num);
